2a6d10ad716dd4d7e24bdde660349c2cc9e9231a
Positive1a17 <Positive1a17@gmail.com>
2025-03-31 21:35:17 +0300

    Полностью обновил конфигурацию сборки

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,16 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
     }
 }
 
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 tasks.register("clean", Delete::class) {
